476d4cc5fdd91d5d430cd4bf1db4791f30545261
imdebamrita <imdebamrita@gmail.com>
2023-04-21 03:23:58 +0530

    shadow in message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,45 +1,54 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 import dayjs from "dayjs";
-import relativeTime from 'dayjs/plugin/relativeTime';
+import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
 const Message = ({ message }) => {
-
-    const isMyMessage = () => {
-        return message.user.id == 'u1';
-    }
+  const isMyMessage = () => {
+    return message.user.id == "u1";
+  };
 
   return (
-    <View style={[
-        styles.container, 
+    <View
+      style={[
+        styles.container,
         {
-            backgroundColor: isMyMessage() ? '#d9fdd3' : 'white',   //#d9fdd3(by my own extension)  #DCF8C5
-            alignSelf: isMyMessage() ? 'flex-end' : 'flex-start',
-            borderBottomLeftRadius: isMyMessage() ? 10 : 0,
-            borderBottomRightRadius: isMyMessage() ? 0 : 10,
-        }
-    ]}>
-      <Text>{ message.text }</Text>
-      <Text style={styles.time}>{ dayjs(message.createdAt).fromNow(true) }</Text>
+          backgroundColor: isMyMessage() ? "#d9fdd3" : "white", //#d9fdd3(by my own extension)  #DCF8C5
+          alignSelf: isMyMessage() ? "flex-end" : "flex-start",
+          borderBottomLeftRadius: isMyMessage() ? 10 : 0,
+          borderBottomRightRadius: isMyMessage() ? 0 : 10,
+        },
+      ]}
+    >
+      <Text>{message.text}</Text>
+      <Text style={styles.time}>{dayjs(message.createdAt).fromNow(true)}</Text>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        // backgroundColor: 'white',
-        // alignSelf: 'flex-start',
-        margin: 10,
-        padding: 10,
-        borderRadius: 10,
-        maxWidth: '80%',
+  container: {
+    // backgroundColor: 'white',
+    // alignSelf: 'flex-start',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    maxWidth: "80%",
+    //shadow
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    time: {
-        color: 'gray',
-        alignSelf: 'flex-end'
-    },
-})
+    shadowOpacity: 0.16,
+    shadowRadius: 1.51,
+    elevation: 2,
+  },
+  time: {
+    color: "gray",
+    alignSelf: "flex-end",
+  },
+});
 
 export default Message;
