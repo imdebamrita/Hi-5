d5defcf17cfb2125c02c0d8a1faaf4104feae7a1
imdebamrita <imdebamrita@gmail.com>
2023-04-21 01:55:44 +0530

    chats screen

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatListItem from './src/components/chatListItems';
+// import ChatListItem from './src/components/chatListItems';
+import ChatsScreen from './src/screens/ChatsScreen';
 
 
-const chat = {
-  "id": "1",
-  "user": {
-    // "id": "u2",
-    "name": "Lukas",
-    "image": "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg"
-  },
-  "lastMessage": {
-    // "id": "m1",
-    "text": "Well done this sprint, guys!",
-    "createdAt": "07.08"
-  }
-}
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <ChatListItem chat={chat}/>
+      <ChatsScreen/>
 
       <StatusBar style="auto" />
     </View>
@@ -31,7 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 45,
   },
 });
